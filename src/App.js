b97daecd8074916b7a2aff95270ec319fecf1203
Bergi <info@bergstreisser.de>
2024-07-24 14:52:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import "../src/scss/main.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
